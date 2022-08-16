7c116c869c6aeb18918cf1313ceedeead63aa22e
Sparrow Estes <sparrow.estes1@phoenix.edu>
2022-08-16 14:04:38 -0700

    initial commit

---
+++ src/main/java/com/sparrowestes/springboot/controller/EmployeeController.java
@@ -0,0 +1,36 @@
+package com.sparrowestes.springboot.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sparrowestes.springboot.model.Employee;
+import com.sparrowestes.springboot.repository.EmployeeRepository;
+
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
+@RequestMapping("/api/v1/")
+public class EmployeeController {
+
+	@Autowired
+	private EmployeeRepository employeeRepository;
+	
+	//get all employees
+	@GetMapping("/employees")
+	public List<Employee> getAllEmployees() {
+		return employeeRepository.findAll();
+	}
+	
+	//create employee rest api
+	@PostMapping("/employees")
+	public Employee createEmployee(@RequestBody Employee employee) {
+		return employeeRepository.save(employee);
+	}
+	
+}
